c089167f10fa94ef38cd704f3676625fa832aa37
AndreyAffhausen <andrey@affhausen.com>
2025-10-02 21:04:23 +0800

    changes

--- web/src/pages/CreateDocumentPage/types/ICreateDocumentPageContext.ts
+++ web/src/pages/CreateDocumentPage/types/ICreateDocumentPageContext.ts
@@ -1,11 +1,15 @@
 import { IUrlScrapingFormData } from '../CreateDocumentPageContainer/UrlScrapingForm/IUrlScrapingForm';
 import { IFileUploadFormData } from '../CreateDocumentPageContainer/FileUploadForm/IFileUploadForm';
+import { ITextPromptFormData } from '../CreateDocumentPageContainer/TextPromptForm/ITextPromptForm';
 
 export interface ICreateDocumentPageHandlers {
   handleGoBack: () => void;
   handleCreateFromUrl: (data: IUrlScrapingFormData) => Promise<void>;
   handleCreateFromFile: (data: IFileUploadFormData) => Promise<void>;
+  handleCreateFromTextPrompt: (data: ITextPromptFormData) => Promise<void>;
   isLoading: boolean;
+  isTextPromptLoading: boolean;
+  textPromptProgress: number;
   error: string | null;
 }
 
